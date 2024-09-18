f88cdf95f8ae5a44e529b27767071a6321d7b211
BhavnaDigihost <bhavana.langi@digihost.in>
2024-09-18 17:04:40 +0530

    vercel file

---
+++ vitebanking/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from './Header.jsx'
+function App() {
+
+	return (
+		<>
+			<h2>Vite Banking</h2>
+			<Header></Header>
+		</>
+	)
+}
+
+export default App
---
+++ vitebanking/src/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <>
+        <h6>Header</h6>
+    </>
+  )
+}
+
+export default Header
